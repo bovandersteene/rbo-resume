4b7bf4a2076add66d680b785c6eadf39474a6f72
bovandersteene <bo.vandersteene@gmail.com>
2019-10-11 11:15:48 +0200

    step 3: model preparation

---
+++ apps/api/src/app/data/demo.ts
@@ -0,0 +1,35 @@
+import { Education, Job, Summary, Technology } from '../model';
+
+export const educations: Education[] = [
+  {
+    course:
+      'NestJS Workshop: Create a backend and frontend app in no time with NestJS and Angular',
+    school: 'Javascript conference',
+    startDate: new Date()
+  }
+];
+
+export const technologies: Technology[] = [
+  { name: 'Javascript', level: 5 },
+  { name: 'Angular', level: 4 },
+  { name: 'NesJs', level: 3 }
+];
+
+export const jobs: Job[] = [
+  {
+    current: true,
+    title: 'Frontend Software Engineer',
+    company: 'reibo.io',
+    description: 'Develop and consult some awesome frontend code',
+    startDate: new Date(2016, 1, 1),
+    technologies: technologies
+  }
+];
+
+export const summary: Summary = {
+  firstName: 'Bo',
+  name: 'Vandersteene',
+  email: 'bo@reibo.io',
+  description:
+    'Bo is a software engineer currently living in Belgium. She likes writing code and solve complex problems. She is passionate about front-end, testing and the latest trends in Computer Science. When not doing geek things, Bo spend some time with her husband and two kids, or fighting fires (she is also a volunteer firefighter).'
+};
---
+++ apps/api/src/app/technology/technology.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TechnologyController } from './technology.controller';
+import { TechnologyService } from './technology/technology.service';
+import { TechnologyService } from './technology.service';
+import { TechnologyService } from './technology.service';
+
+@Module({
+  controllers: [TechnologyController],
+  providers: [TechnologyService]
+})
+export class TechnologyModule {}
