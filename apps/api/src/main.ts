72f2f99c62078bb14ec5a923ba43ccfe143b72e5
bovandersteene <bo.vandersteene@gmail.com>
2019-10-11 11:15:48 +0200

    step 2: add swagger configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,11 +6,29 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { environment } from './environments/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  if(!environment.production) {
+    const options = new DocumentBuilder()
+      .setTitle('Resume api')
+      .setDescription('The resume API description')
+      .setVersion('1.0')
+      .setBasePath('api')
+      .addTag('resume')
+      .addTag('example')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
+  }
+
   const port = process.env.port || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
